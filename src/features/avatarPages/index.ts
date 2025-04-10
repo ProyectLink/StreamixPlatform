72d9e064854e982d8049ced17b3ee1504ab5007d
Conitoweb <daniel2905200@gmail.com>
2025-04-10 11:53:33 -0300

    adding autoBarrel-avatarPages

--- src/features/avatarPages/index.ts
+++ src/features/avatarPages/index.ts
@@ -1,3 +1,5 @@
 export * from './AvatarPages';
 export * from './components/avatarItems/AvatarItems';
 export * from './components/avatarList/AvatarList';
+export * from './data/avatar';
+export * from './props/index';
