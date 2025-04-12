c050abf58065056894fdf93bff97560a4c7358f5
danielbenitez <daniel29052003@gmail.com>
2025-04-12 11:36:52 -0300

    adding : autoBarrel

--- src/features/avatarPages/index.ts
+++ src/features/avatarPages/index.ts
@@ -1,4 +1,5 @@
 export * from './AvatarPages';
+export * from './components/avatarAdd/AvatarAdd';
 export * from './components/avatarItems/AvatarItems';
 export * from './components/avatarList/AvatarList';
 export * from './data/avatar';
